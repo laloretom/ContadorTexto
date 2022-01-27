29866f1e367a3b25aea8bb0f35894c67b0cdad37
Luis Loreto <a216207287@unison.mx>
2022-01-26 20:55:22 -0700

    Initial commit

---
+++ src/main/java/WordCounterSplit.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.regex.Pattern;
+
+public class WordCounterSplit {
+    public static void main(String args[]) {
+
+        if (args.length == 0) {
+            System.out.println("Falta el nombre del archivo");
+            System.exit(1);
+        }
+
+        String fileName = args[0];
+
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("El nombre del archivo no se encuentra");
+            System.exit(2);
+        }
+
+       BufferedReader in = new BufferedReader(fileReader);
+
+        Pattern pat = Pattern.compile("[\\s]*[0-9]*[0-9]+");
+
+        String textLine = null;
+        int contadorPalabras = 0;
+        int contadorLineas = 0;
+        String linea;
+
+        // tiempo al inicio
+        long start = System.currentTimeMillis();
+        try {
+            while ((linea = in.readLine()) !=null){
+                String[] palabras = linea.split("\\s+");
+                contadorPalabras = contadorPalabras + palabras.length;
+                contadorLineas++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo de entrada");
+            System.exit(3);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // tiempo de procesamiento
+        long time = System.currentTimeMillis() - start;
+
+        System.out.printf("El archivo %s tiene %,7d palabras. " , fileName, contadorPalabras);
+        System.out.printf(" Numero de lineas: %,6d%n", contadorLineas);
+        System.out.printf("Tiempo procesamiento (milisegundos): %d %n" , time);
+    }
+}
---
+++ src/main/java/WordCounterStringTokenizer.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class WordCounterStringTokenizer {
+    public static void main(String args[]) {
+
+        if (args.length == 0) {
+            System.out.println("Falta el nombre del archivo");
+            System.exit(1);
+        }
+
+        String fileName = args[0];
+
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("El nombre del archivo no se encuentra");
+            System.exit(2);
+        }
+
+       BufferedReader in = new BufferedReader(fileReader);
+
+
+        String textLine = null;
+        int contadorPalabras = 0;
+        int contadorLineas = 0;
+
+        String linea;
+        // tiempo al inicio
+        long start = System.currentTimeMillis();
+        try {
+            while ((linea = in.readLine()) !=null){
+                StringTokenizer st= new StringTokenizer(linea);
+                contadorPalabras=contadorPalabras + st.countTokens();
+                contadorLineas++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo de entrada");
+            System.exit(3);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // tiempo de procesamiento
+        long time = System.currentTimeMillis() - start;
+
+        System.out.printf("El archivo %s tiene %,7d palabras. " , fileName, contadorPalabras );
+        System.out.printf(" Numero de lineas: %,6d%n", contadorLineas);
+        System.out.printf("Tiempo procesamiento (milisegundos): %d %n" , time);
+    }
+}
